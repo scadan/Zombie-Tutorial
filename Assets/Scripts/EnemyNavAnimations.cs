1d92c0acf314fff1975d824a243c93b721d6b760
scadan <aidan.yates.7@gmail.com>
2018-11-12 14:34:53 +0000

    NavMeshAgent
    Added NavMesh to the zombie :)

---
+++ Assets/Scripts/EnemyNavAnimations.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNavAnimations : MonoBehaviour {
+
+    NavMeshAgent agent;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        anim = GetComponentInParent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(agent.velocity.magnitude);
+        if(agent)
+        {
+            anim.SetFloat("Speed", agent.velocity.magnitude);
+        }
+		
+	}
+}
--- Assets/Scripts/EnemyNavMovement.cs
+++ Assets/Scripts/EnemyNavMovement.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyNavMovement : MonoBehaviour {
 
+    NavMeshAgent agent;
+
+    [SerializeField]
+    private GameObject target;
+
 	// Use this for initialization
 	void Start () {
+        
+
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(target.transform.position);
+
+        if(agent.remainingDistance<(agent.stoppingDistance+0.5f))
+        {
+            transform.LookAt(target.transform);
+        }
 	}
 }

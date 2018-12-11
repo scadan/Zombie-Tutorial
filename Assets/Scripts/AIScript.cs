89c19e53159ee7e61114903149a29ce4cd1372b3
scadan <aidan.yates.7@gmail.com>
2018-12-11 11:25:09 +0000

    Finished Structoring the State Based AI

--- Assets/Scripts/AIScript.cs
+++ Assets/Scripts/AIScript.cs
@@ -9,9 +9,20 @@ public class AIScript : MonoBehaviour {
     public Behaviours currBehaviour = Behaviours.Patrol;
 
     NavMeshAgent agent;
+
+    public Health healthScript;
+
+
+
+    public Transform player, healthPoint;
+
+    public int chaseDistance;
+
+    public int findDistance;
+    
     //List of Array of Points- starts at number 0
     public GameObject waypoints;
-    Transform[] points;
+    public GameObject[] points;
     public int destPoint = 0;
 
     void RunBehaviours()
@@ -34,11 +45,11 @@ public class AIScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        points = waypoints.GetComponentInChildren<Transform>();
-        agent = GetComponent<NavMeshAgent>();
-        //can turn off auto braking to not stop between waypoints
-        agent.autoBraking = false;
-        GoToNextPoint();
+        healthScript = GetComponent<Health>();
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        healthPoint = GameObject.FindGameObjectWithTag("PickUp").transform;
+        points = GameObject.FindGameObjectsWithTag("Waypoint");
     }
 	
 	// Update is called once per frame
@@ -51,17 +62,41 @@ public class AIScript : MonoBehaviour {
 
     void RunPatrolState()
     {
-
+        if (Vector3.Distance(transform.position, player.position) < findDistance)
+        {
+            currBehaviour = Behaviours.Combat;
+        }
+        else if (agent.remainingDistance < 0.5f)
+        {
+            if (points.Length == 0)
+            {
+                return;
+            }
+            agent.SetDestination(points[destPoint].transform.position);
+            //if goes higher than the total number of waypoints -> go back to start of array
+            destPoint = (destPoint + 1) % points.Length;
+        }
     }
 
     void RunHealState()
     {
-
+        agent.SetDestination(healthPoint.position);
+        if (agent.remainingDistance < 0.1f)
+        {
+            currBehaviour = Behaviours.Patrol;
+        }
     }
 
     void RunCombatState()
     {
-
+        if (Vector3.Distance(transform.position, player.position) < chaseDistance)
+        {
+            currBehaviour = Behaviours.Patrol;
+        }
+        else
+        {
+            agent.SetDestination(player.position);
+        }
     }
 
     void GoToNextPoint()
@@ -71,8 +106,6 @@ public class AIScript : MonoBehaviour {
             return;
         }
 
-        agent.SetDestination(points[destPoint].position);
-        //if goes higher than the total number of waypoints -> go back to start of array
-        destPoint = (destPoint + 1) % points.Length;
+       
     }
 }

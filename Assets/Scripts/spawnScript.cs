498f4d22cfb581c15450ce4ddb62f95f49b7428a
scadan <aidan.yates.7@gmail.com>
2018-12-14 11:42:13 +0000

    I have added a spawn point to spawn more zombies

---
+++ Assets/Scripts/spawnScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnScript : MonoBehaviour {
+
+    [SerializeField] GameObject thingToSpawn;
+    [SerializeField] float delayBetweenSpawn = 2.0f;
+    [SerializeField] float timeOfNextSpawn = 1f;
+
+    int amountToSpawn = 10;
+    static int amountSpawned = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Time.time >= timeOfNextSpawn && amountSpawned < amountToSpawn)
+        {
+            Instantiate(thingToSpawn, transform.position, Quaternion.identity);
+            timeOfNextSpawn = Time.time + delayBetweenSpawn;
+            amountSpawned++;
+        }
+		
+	}
+}

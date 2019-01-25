faeefce7834c25efed14f444b7aa287d005daf4d
scadan <31972143+scadan@users.noreply.github.com>
2019-01-25 14:06:51 +0000

    Fixed the PickUp feature. So now the player can properly upgrade his weapon

--- Assets/Scripts/shootingScript.cs
+++ Assets/Scripts/shootingScript.cs
@@ -80,7 +80,7 @@ public class shootingScript : MonoBehaviour {
 
 	}
 
-    private void OnCollisionEnter(Collision collision) {
+    private void OnTriggerEnter(Collider collision) {
         if (collision.gameObject.tag == "BulletPickup") {
             damageDealt += 20;
 
